89c462b08d4ae33d3bee720677314519946d5f79
Matylda <matylda.badowska@gmail.com>
2020-12-14 15:56:13 +0100

    BIW-5

--- src/main/java/pl/book/it/web/config/WebConfiguration.java
+++ src/main/java/pl/book/it/web/config/WebConfiguration.java
@@ -3,10 +3,13 @@ package pl.book.it.web.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -16,10 +19,20 @@ public class WebConfiguration implements WebMvcConfigurer {
     //logowanie powinno być zrobione na tockenie, w ty momencoe hasło przesyłane jest restem co nie jest idealne rozwiązanie
     @Bean
     public RestTemplate restTemplate(ApiUserConfig apiUserConfig) {
-        return new RestTemplateBuilder()
+        final RestTemplate restTemplate = new RestTemplateBuilder()
                 .basicAuthentication(apiUserConfig.getUserName(), apiUserConfig.getPassword())
                 .messageConverters(new MappingJackson2HttpMessageConverter())
                 .build();
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        stringHttpMessageConverter.setWriteAcceptCharset(true);
+        for (int i = 0; i < restTemplate.getMessageConverters().size(); i++) {
+            if (restTemplate.getMessageConverters().get(i) instanceof StringHttpMessageConverter) {
+                restTemplate.getMessageConverters().remove(i);
+                restTemplate.getMessageConverters().add(i, stringHttpMessageConverter);
+                break;
+            }
+        }
+        return restTemplate;
     }
 
     @Override
